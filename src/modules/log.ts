8f7d851ac093c26ee704533f5c5098e4fe36840b
xingqiankun <1911898144@qq.com>
2022-01-10 13:03:27 +0800

    创建log类

--- src/modules/log.ts
+++ src/modules/log.ts
@@ -1,9 +1,43 @@
-interface LoggerI {
+export interface LoggerOutPutI {
   info(...args:any):void
   warn(...args:any):void
   error(...args:any):void
 }
-interface LoggerConfig{}
-
-export default class Logger {
+export enum LOG_TYPE{
+  info = "INFO",
+  warn = "WARN",
+  error = "ERROR"
 }
+
+
+export interface LoggerConfig{}
+
+export default class Logger implements LoggerOutPutI{
+  config = {
+    time: true,
+  }
+  private log(type: LOG_TYPE, ...args: any): void {
+    let time = new Date().toLocaleString();
+    switch(type){
+      case LOG_TYPE.info:
+        console.log(`[${time} %c ${type}]: `,...args)
+        break;
+      case LOG_TYPE.warn:
+        console.log(`[${time} %c ${type}]: `,...args)
+        break;
+      case LOG_TYPE.error:
+        console.log(`[${time} %c ${type}]: `,...args)
+        break;
+      default:
+        break;
+    }
+  }
+  info(...args: any): void {
+    this.log(LOG_TYPE.info,...args)
+  }
+  warn(...args: any): void {
+    this.log(LOG_TYPE.warn,...args)
+  }
+  error(...args: any): void {
+    this.log(LOG_TYPE.error,...args)
+  }}
---
+++ test/test.ts
@@ -0,0 +1,5 @@
+import Logger from '../dist/modules/log'
+
+const logger = new Logger()
+
+logger.info("asdfafsasd")
